23b49aced30ae3ef4049c330322f59beb3cd081a
Mastermind-U <rex49513@gmail.com>
2022-01-09 01:41:26 +0300

    added crypto config

--- app/main.py
+++ app/main.py
@@ -29,7 +29,14 @@ async def make_order(data: OrderWebhookData):
         "timenow": "{{timenow}}"
     }
     """
-    # alpaca.post_order(OrderPlaceRequest(
-    #     extended_hours=None
-    # ))
-    return data
+    return await alpaca.post_order(OrderPlaceRequest(
+        symbol=data.ticker,
+        notional=1000,
+        side="buy",
+        type="limit",
+        time_in_force="gtc",
+        limit_price=data.close,
+        order_class="simple",
+        take_profit=Profit(limit_price=data.close * 1.01),
+        stop_loss=StopLoss(stop_price=data.close * .98),
+    ))
