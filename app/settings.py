ee29815bf235fb9c43d2d6ec3ff36633fd78676a
Mastermind-U <rex49513@gmail.com>
2022-01-09 01:41:54 +0300

    removed secret due to decode error

--- app/settings.py
+++ app/settings.py
@@ -2,15 +2,15 @@
 
 from functools import lru_cache
 
-from pydantic import BaseSettings, SecretStr
+from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
     """Base settings model."""
 
     DEBUG: bool = True
-    ALPACA_API_KEY_ID: SecretStr
-    ALPACA_SECRET_KEY: SecretStr
+    ALPACA_API_KEY_ID: str
+    ALPACA_SECRET_KEY: str
 
 
 @lru_cache
